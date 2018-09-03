993e2027759cf539125e19b4c7cbc27e30ca47fd
Sergey Chernetsov <schernetsov@gmail.com>
2018-09-03 19:10:29 +0300

    add table

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import ButtonAppBar from "./components/ButtonAppBar";
 import SimpleTable from "./components/SimpleTable";
+import EnhancedTable from "./components/EnhancedTable";
 import MediaCard from "./components/MediaCard";
 import "./App.css";
 
@@ -11,6 +12,7 @@ class App extends Component {
         <ButtonAppBar />
         <MediaCard />
         <SimpleTable />
+        <EnhancedTable />
       </div>
     );
   }
