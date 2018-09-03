299e0408da9b26a4f6ecce544688d057b4178c6e
Sergey Chernetsov <schernetsov@gmail.com>
2018-09-03 19:20:49 +0300

    add table data

--- src/components/EnhancedTable.js
+++ src/components/EnhancedTable.js
@@ -20,9 +20,9 @@ import FilterListIcon from '@material-ui/icons/FilterList';
 import { lighten } from '@material-ui/core/styles/colorManipulator';
 
 let counter = 0;
-function createData(name, calories, fat, carbs, protein) {
+function createData(phone, address, balance) {
   counter += 1;
-  return { id: counter, name, calories, fat, carbs, protein };
+  return { id: counter, phone, address, balance };
 }
 
 function desc(a, b, orderBy) {
@@ -197,7 +197,16 @@ class EnhancedTable extends React.Component {
       createData("79859550222", "0xB512a84B2BE31d12C7E6c6ca3CBb86E083f0017b", 22),
       createData("76857656333", "0xb5181242CD2790beDc06e1d75DCe7C8420c24887", 30),
       createData("78975634444", "0xFbaa2E068141903aC774D91a14DBBee7Bd5d8db5", 4),
-      createData("76853333555", "0x780A59C220Be88700067E1EE00d68D91D4253003", 21)
+      createData("76853333555", "0x780A59C220Be88700067E1EE00d68D91D4253003", 212),
+      createData("79636366336", "0x39aeD435E97F0851811bd0f39870Fbc2e005fE49", 111),
+      createData("79836377777", "0x385acA76c2085f5F7364BF40Fa9d1cf108c180D8", 2332),
+      createData("76857688888", "0x174CD344Eef125C2dbAdec0A0736E9C39C40a765", 3550),
+      createData("78975638989", "0xfe4Ad76814046Ba814749B1E9f53D6BcD6A3A5a9", 466),
+      createData("76853300000", "0x7FF826F3a620c3649A3c38E9AFC8262D0aAD7C7F", 271),
+      createData("79110343211", "0xAa6A1Cc9e89907a441ef4C72f464c092bc1D11c8", 2),
+      createData("79859544422", "0x611ba0Ddd7C2C06df23EfA051cA30c32cF61F9FF", 223),
+      createData("76857655533", "0x93731529ae33579FCC8AFe38D0E82167e9bB2e5c", 241),
+      createData("78975664444", "0x1cec71A44004E770D71Cbeb80BbFF0Fb3A56C8b2", 42),
     ],
     page: 0,
     rowsPerPage: 5,
@@ -291,10 +300,10 @@ class EnhancedTable extends React.Component {
                         <Checkbox checked={isSelected} />
                       </TableCell>
                       <TableCell component="th" scope="row" padding="none">
-                        {n.name}
+                        {n.phone}
                       </TableCell>
-                      <TableCell numeric>{n.calories}</TableCell>
-                      <TableCell numeric>{n.fat}</TableCell>
+                      <TableCell numeric>{n.address}</TableCell>
+                      <TableCell numeric>{n.balance}</TableCell>
                     </TableRow>
                   );
                 })}
